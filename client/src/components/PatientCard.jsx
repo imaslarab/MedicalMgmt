014665a6d68c706442a8dd9b3baba7520df8f8b3
imaslarab <baralsami@gmail.com>
2020-03-27 14:41:38 -0400

    Add edit and delete buttton

--- client/src/components/PatientCard.jsx
+++ client/src/components/PatientCard.jsx
@@ -14,8 +14,16 @@ class PatientCard extends Component {
 		return (
             <div className="col-xl-3 col-md-6 mb-4">
                 <div className="card shadow mb-4">
-                    <div className="card-header py-3">
+                    <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                         <h6 className="m-0 font-weight-bold text-primary">{patient.name}</h6>
+                        <div>
+                            <a href="#" className="btn btn-info btn-circle mr-2">
+                                <i className="fa fa-edit"></i>
+                            </a>
+                            <a href="#" className="btn btn-danger btn-circle">
+                                <i className="fa fa-trash-o"></i>
+                            </a>
+                        </div>
                     </div>
                     <div className="card-body">
                         <p>Patient Id: {patient.patientId}</p>
