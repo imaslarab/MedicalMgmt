52aad175c115e5c4fcbb7d1cb1139309fa4ac535
imaslarab <baralsami@gmail.com>
2020-03-27 03:03:41 -0400

    Minor fix

--- client/src/components/MainSidebar.jsx
+++ client/src/components/MainSidebar.jsx
@@ -34,21 +34,21 @@ class MainSidebar extends Component {
         <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
             <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div className="sidebar-brand-icon">
-                <i className="fa fa-user-md"></i>
+                <i className="fa fa-hospital-o"></i>
                 </div>
                 <div className="sidebar-brand-text mx-3">Main Dash</div>
             </a>
             <hr className="sidebar-divider my-0"/>
             <li className="nav-item">
                 <a className="nav-link" href="/appointments">
-                <i className="fa fa-fw fa-tachometer-alt"></i>
+                <i className="fa fa-calendar-o fa-fw"></i>
                 <span>Appointments</span></a>
             </li>
 
             <hr className="sidebar-divider my-0"/>
             <li className="nav-item">
                 <a className="nav-link" href="/patients">
-                <i className="fa fa-fw fa-tachometer-alt"></i>
+                <i className="fa fa-users fa-fw"></i>
                 <span>Patients</span></a>
             </li>
             <hr className="sidebar-divider d-none d-md-block" />
