7d0f793565c941a7b8293bc9bba6c171c17e6638
imaslarab <baralsami@gmail.com>
2020-04-27 15:20:23 -0400

    Add appointment modal

--- client/src/views/AppointmentPage.jsx
+++ client/src/views/AppointmentPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import AppointmentCard from '../components/AppointmentCard';
+import AddAppointmentModal from '../components/modals/AddAppointmentModal';
 // import PatientApi from '../api/patient';
 
 class AppointmentPage extends Component {
@@ -8,8 +9,19 @@ class AppointmentPage extends Component {
         super(props);
         this.state = {
             isLoading: false,
-            appointments: []
+            appointments: [],
+            isAddAppointmentModalOpen:false
         };
+        this.openAddAppointmentModal = this.openAddAppointmentModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    openAddAppointmentModal() {
+        this.setState({isAddAppointmentModalOpen: true});
+    }
+
+    closeModal() {
+        this.setState({isAddAppointmentModalOpen: false});
     }
 
     componentDidMount() {
@@ -19,16 +31,18 @@ class AppointmentPage extends Component {
     }
 
     render() {
-        let {isLoading, appointments} = this.state;
+        let {isLoading, appointments, isAddAppointmentModalOpen} = this.state;
 
         const cardsList = [];
         
         let patients = [
-            {patientId:1, name:"John Trump", age:28},
-            {patientId:2, name:"Mickey Barber", age:15},
-            {patientId:3, name:"Donald Tomgato", age:21},
-            {patientId:4, name:"Sean Mars", age:50},
-            {patientId:5, name:"Adam Newton"}
+            {patientId:1, patientName:"John Trump", age:28, sex:"Male", phoneNumber:'920922029', address:''},
+            {patientId:2, patientName:"Mickey Barber", age:15, sex:"Male", phoneNumber:'720822029', address:''},
+            {patientId:3, patientName:"Donald Tomgato", age:21, sex:"Male", phoneNumber:'900208299', address:''},
+            {patientId:4, patientName:"Sean Mars", age:50, sex:"Male", phoneNumber:'220192029', address:''},
+            {patientId:5, patientName:"Adam Newton", age:35, sex:"Male", phoneNumber:'09111919', address:''},
+            {patientId:6, patientName:"Monica Geller", age:40, sex:"Female", phoneNumber:'12302029', address:''},
+            {patientId:7, patientName:"Rita Bing", age:50, sex:"Female", phoneNumber:'920922029', address:''},
         ];
         appointments = [
             {appointmentId:1, date:"March 27, 200", time:"3:00pm", patient:patients[0]},
@@ -46,12 +60,16 @@ class AppointmentPage extends Component {
         return (
             <div id="content">
                 <div className="container-fluid">
-                <h1 className="h3 mb-4 text-gray-800">Appointment List</h1>
+                <h1 className="h3 mb-4 text-gray-800">
+                    Appointment List
+                    <a href="#" onClick={this.openAddAppointmentModal} className="btn btn-primary ml-3"><i className="fa fa-plus"></i></a>
+                    </h1>
                 { isLoading ? 
                     <h1 className="h3 mb-4 text-gray-800">Loading ..</h1>
                     : <div className="row">{cardsList}</div>
                 }
                 </div>
+                <AddAppointmentModal isModalOpen={isAddAppointmentModalOpen} closeModal={this.closeModal} />
             </div>
 		);
 	}
