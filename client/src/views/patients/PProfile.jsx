f669668e03b48df8bea4174a100724916734fd2e
imaslarab <baralsami@gmail.com>
2020-05-10 18:38:43 -0400

    Add patient profile

--- client/src/views/patients/PProfile.jsx
+++ client/src/views/patients/PProfile.jsx
@@ -4,29 +4,44 @@ import AppointmentCard from '../../components/AppointmentCard';
 import BillingInfoCard from '../../components/BillingInfoCard';
 import DiagnosisCard from '../../components/DiagnosisCard';
 
-import PatientApi from '../../api/patient';
-import App from '../../App';
-
 import patientImg from '../../assets/images/patient.svg';
+
+import PatientApi from '../../api/patient';
+import AuthService from '../../services/AuthService';
+import {utilService} from '../../services/UtilService';
+
+import EditPatientModal from '../../components/modals/EditPatientModal';
 
 class PProfile extends Component {
     constructor(props) {
         super(props);
         this.state = {
             isLoading: false,
-            patient: {
-                patientId:1, patientName:"Sami Baral", age:25, sex:"Female",
-                phone:'220922029',
-                address: "Worcester MA"
-            }
+            patient: {},
+            isEditPatientModalOpen: false
         };
+
+        this.openEditPatientModal = this.openEditPatientModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    openEditPatientModal() {
+        this.setState({isEditPatientModalOpen: true});
+    }
+
+    closeModal() {
+        this.setState({isEditPatientModalOpen:false});
     }
 
     componentDidMount() {
+        let patientId = AuthService.getUserId();
+        PatientApi.getPatient(patientId, (response)=> {
+            this.setState({...this.state, isLoading:false, patient:response});
+        });
     }
 
     render() {
-        let {isLoading, patient} = this.state;
+        let {isLoading, patient, isEditPatientModalOpen} = this.state;
 
         return (
             <div id="content">
@@ -36,14 +51,19 @@ class PProfile extends Component {
                             <img className="img-profile rounded-circle profile-img" src={patientImg} alt="" />
                         </div>
                         <div className="col-md-10 profile-detail">
-                            <h1 className="h3 text-gray-800">{patient.patientName}</h1>
+                            <h1 className="h3 text-gray-800">{patient.patientName} 
+                                <a href="#" className="btn btn-info btn-circle ml-2" onClick={this.openEditPatientModal}>
+                                    <i className="fa fa-edit"></i>
+                                </a>
+                            </h1>
                             <p>Patient ID: {patient.patientId}</p>
-                            <p><span>Age: {patient.age}</span><span>Sex: {patient.sex}</span></p>
+                            <p><span>Date of Birth: {utilService.formatDateString(patient.dob)}</span><span>Sex: {patient.sex}</span></p>
                             <p>Phone number: {patient.phone}</p>
                             <p>Address: {patient.address}</p>
                         </div>
                     </div>
                 </div>
+                <EditPatientModal patient={patient} doctor={null} isModalOpen={isEditPatientModalOpen} closeModal={this.closeModal}/>
             </div>
 		);
 	}
